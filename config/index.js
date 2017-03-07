a74888f5d02323d682f734459b8de643a99e252b
what <Administrator@2013-20160513BX>
2017-03-07 18:44:34 +0800

    update code

--- config/index.js
+++ config/index.js
@@ -27,12 +27,16 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/zhuweilai/api': {
+        target: 'http://61.155.169.165:8089/zhuweilai/api'
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,39 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+
+  let setArr = function () {
+    let result = [];
+    let minVal = 2;
+    let maxVal = 32;
+    for (let i = minVal; i < maxVal; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  let fn = function (n) {
+    let result = [];
+    let arr = setArr();
+    for (let i = 0; i < n; i++) {
+      var index = Math.floor(Math.random() * arr.length);
+      result.push(arr[index]);
+      arr.splice(index, 1);
+    }
+    return result;
+  }
+  console.log(fn(30));
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
